4d27aed3d99c364ba0e8218127458246ff0a791d
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-11-07 11:25:14 -0500

    feature: added fonts

--- app/posts/layout.jsx
+++ app/posts/layout.jsx
@@ -1,8 +1,12 @@
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['400']  });
+
 function PostsLayout({ children }) {
 
   return (
     <>
-      <small>Home &bull; Posts</small>
+      <small className={`${poppins.className}`}>Home &bull; Posts</small>
       <section>
         { children }
       </section>
