f53a1cf1e4eb238c3b116db9d26a1cb216d72737
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-11-07 14:56:19 -0500

    feature: added next images

--- app/posts/[id]/comments/page.jsx
+++ app/posts/[id]/comments/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 const fetchPostComments = async ( id ) => {
   await new Promise( resolve => setTimeout( resolve, 5000))
   // throw new Error('Error en la carga de comentarios')
@@ -21,6 +23,9 @@ async function postComment( { params } ) {
         comments.map( ({ name, body }, index) => {
           return (
               <li key={ index }>
+                <Image height={100} width={100}
+  src={`https://i.pravatar.cc/300`}
+  alt={`avatar`} />
                 <p className="text-center text-xl">{ name } </p>
                 <p className="my-2">{ body }</p>
               </li>
--- app/posts/layout.jsx
+++ app/posts/layout.jsx
@@ -1,6 +1,6 @@
 import { Poppins } from "next/font/google"
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400']  });
+const poppins = Poppins({ subsets: ['latin'], weight: ['400'], variable: '--font-poppins'  });
 
 function PostsLayout({ children }) {
 
